8cb3327c29d7cce7324ab60c199a4b307387be02
Telman Islamov <t.islamov@aladdin.ru>
2025-04-16 18:11:21 +0300

    implemented the 'Personalization' interface

--- src/Main.java
+++ src/Main.java
@@ -3,18 +3,22 @@ import javacard.framework.Applet;
 import javacard.framework.ISO7816;
 import javacard.framework.ISOException;
 import javacard.security.CryptoException;
+import org.globalplatform.Personalization;
 
-public class Main extends Applet {
+public class Main extends Applet implements Personalization {
 
-    private Main(byte[] parameters, short offset, byte length) {
+    private
+    Main(byte[] parameters, short offset, byte length) {
         register(parameters, (short) (offset + 1), parameters[offset]);
     }
 
-    public static void install(byte[] parameters, short offset, byte length) {
+    public static void
+    install(byte[] parameters, short offset, byte length) {
         new Main(parameters, offset, length);
     }
 
-    public void process(APDU apdu) throws ISOException {
+    public void
+    process(APDU apdu) throws ISOException {
         if (selectingApplet())
             return;
         byte[] buffer = apdu.getBuffer();
@@ -22,5 +26,12 @@ public class Main extends Applet {
             CryptoException.throwIt((short) 0x6666);
         apdu.setIncomingAndReceive();
         apdu.setOutgoingAndSend((short) 0, (short) 11);
+    }
+
+    public short
+    processData(byte[] inBuffer, short inOffset, short inLength, byte[] outBuffer, short outOffset)
+    {
+        /* TODO: add personalization data processing machinery. */
+        return 0x6f00;
     }
 }
