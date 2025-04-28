cdcb9b5457038eda9ba714f2b055f44d43258ec1
Telman Islamov <t.islamov@aladdin.ru>
2025-04-28 10:05:43 +0300

    removed common files

--- build.sh
+++ build.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/bash
 
-export JAVA_HOME=/usr/bin/java
+export JAVA_HOME=$(whereis "java")
 export PATH=$JAVA_HOME/bin:$PATH
 
-export JC_HOME=./libraries/jc304_kit
+export JC_HOME=../Libraries/jc304_kit
 export PATH=$JC_HOME/bin:$PATH
 
 if [ -d build ]; then
@@ -16,17 +16,17 @@ build_dir=./build
 
 applet_name=Main
 package_name=inter.galaxy.me
-gp_lib_dir=./libraries/GP-v171/1.7
+gp_lib_dir=../Libraries/GP-v171/1.7
 
-jc_jar=./libraries/jc304_kit/lib/api_classic.jar
-gp_jar=./libraries/GP-v171/1.7/gpapi-globalplatform.jar
-uicc_jar=./libraries/UICC-API/uicc-api-for-java-card-REL-15_2.jar
-usim_jar=./libraries/USIM-API/usim-api-for-java-card-REL-15_2.jar
+jc_jar=../Libraries/jc304_kit/lib/api_classic.jar
+gp_jar=../Libraries/GP-v171/1.7/gpapi-globalplatform.jar
+uicc_jar=../Libraries/UICC-API/uicc-api-for-java-card-REL-15_2.jar
+usim_jar=../Libraries/USIM-API/usim-api-for-java-card-REL-15_2.jar
 
-jc_exports=./libraries/jc304_kit/api_export_files
-uicc_exports=./libraries/UICC-API/exports
-usim_exports=./libraries/USIM-API/exports
-gp_exports=./libraries/GP-v171/1.7/exports
+jc_exports=../Libraries/jc304_kit/api_export_files
+uicc_exports=../Libraries/UICC-API/exports
+usim_exports=../Libraries/USIM-API/exports
+gp_exports=../Libraries/GP-v171/1.7/exports
 
 # Project-specific export files
 export_files=$gp_exports:$usim_exports:$jc_exports:$uicc_exports
@@ -37,7 +37,7 @@ jar_files=$jc_jar:$gp_jar:$uicc_jar:$usim_jar
 javac -Xlint:-options -Xlint:deprecation -g -d $build_dir -classpath $jar_files -sourcepath ./src \
 -source 1.5 -target 1.5 $source_path
 
-jc_converter=./libraries/jc304_kit/lib/tools.jar
+jc_converter=../Libraries/jc304_kit/lib/tools.jar
 
 java -Djc.home=$JC_HOME -cp $jc_converter com.sun.javacard.converter.Main -i -classdir $build_dir -out CAP EXP JCA -exportpath $export_files \
 -applet 0xA0:0x00:0x00:0x00:0x00:0x00:0x00:0x00 $applet_name \
